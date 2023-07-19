823d635b6f34944874961a4ebb0341dd7b00a436
Ali kazemzadeh <131513515+alikazemzadeh76@users.noreply.github.com>
2023-07-19 09:53:09 +0330

    Update settings.py

--- multi_shop/settings.py
+++ multi_shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#!a1a5brg%5o$)6d0k72k46@xhf*5p&utid4pst=fgv=z&^f%'
+SECRET_KEY = 'SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
